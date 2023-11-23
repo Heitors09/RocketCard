0b9ec59d64405453765bfdce9f1c9aa26c92917e
Heitor <heitorao32@gmail.com>
2023-11-23 15:58:17 -0300

    gitAPI

---
+++ javascript.js
@@ -0,0 +1,64 @@
+const img = document.querySelector('.picture-img')
+const button = document.querySelector('.change-bg')
+const cardContent = document.querySelector('.card')
+
+let actualColor = 0
+
+button.addEventListener('click',()=>{
+    const cardContent = document.querySelector('.card')
+    const color =['#965fd4','#8bd450','#734f9a', '#3f6d4e','#1d1a2f' ]
+
+    cardContent.style.background = color[actualColor]
+    actualColor = (actualColor + 1) % color.length
+
+})   
+
+
+
+
+
+async function getAPI(){
+    const result = await fetch('https://api.github.com/users/Heitors09')
+    const img = document.querySelector('.picture-img')
+    const nameTitle = document.querySelector('.name')
+    const infoPai = document.querySelector('.infos')
+    
+    if (!result){
+        throw new Error(result.status)
+    }else{
+      const data = await  result.json()
+      img.src = data.avatar_url
+      nameTitle.textContent = data.login
+
+
+      const newDiv = document.createElement('ul')
+      newDiv.innerHTML = `<li><svg  xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true" fill="#FFF" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-people">
+          <path fill-rule="evenodd" d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"></path>
+      </svg> ${data.followers} Seguidores</li>
+      <li> <svg  xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true" fill="#FFF" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-people">
+          <path fill-rule="evenodd" d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"></path>
+      </svg> ${data.following} Seguindo</li>
+      <li> <svg xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true" fill="#fff" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo UnderlineNav-octicon hide-sm">
+         <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path>
+     </svg> ${data.public_repos} Repositórios</li>
+      <li> <svg  xmlns="http://www.w3.org/2000/svg" class="octicon octicon-organization" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true" fill="#FFF"><path fill-rule="evenodd" d="M1.5 14.25c0 .138.112.25.25.25H4v-1.25a.75.75 0 01.75-.75h2.5a.75.75 0 01.75.75v1.25h2.25a.25.25 0 00.25-.25V1.75a.25.25 0 00-.25-.25h-8.5a.25.25 0 00-.25.25v12.5zM1.75 16A1.75 1.75 0 010 14.25V1.75C0 .784.784 0 1.75 0h8.5C11.216 0 12 .784 12 1.75v12.5c0 .085-.006.168-.018.25h2.268a.25.25 0 00.25-.25V8.285a.25.25 0 00-.111-.208l-1.055-.703a.75.75 0 11.832-1.248l1.055.703c.487.325.779.871.779 1.456v5.965A1.75 1.75 0 0114.25 16h-3.5a.75.75 0 01-.197-.026c-.099.017-.2.026-.303.026h-3a.75.75 0 01-.75-.75V14h-1v1.25a.75.75 0 01-.75.75h-3zM3 3.75A.75.75 0 013.75 3h.5a.75.75 0 010 1.5h-.5A.75.75 0 013 3.75zM3.75 6a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM3 9.75A.75.75 0 013.75 9h.5a.75.75 0 010 1.5h-.5A.75.75 0 013 9.75zM7.75 9a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM7 6.75A.75.75 0 017.75 6h.5a.75.75 0 010 1.5h-.5A.75.75 0 017 6.75zM7.75 3a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5z"></path></svg> ${data.company}</li>
+      <li> <svg xmlns="http://www.w3.org/2000/svg" class="octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="16" height="16" fill="#FFF" aria-hidden="true"><path fill-rule="evenodd" d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"></path></svg> ${data.location}</li>`
+      
+      
+      
+      
+      infoPai.innerHTML += newDiv.outerHTML
+
+    }
+}
+
+
+
+
+
+
+
+getAPI();
